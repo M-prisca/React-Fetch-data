6a13164da7624eeca92a3e853e58d923bad00a83
Prisca <priscamasereli@gmail.com>
2025-09-01 10:50:48 +0200

    Feat: Add fetching post functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Posts from "./Components/Posts";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello World</h1>
+      <h1 className="text-3xl font-bold text-center">
+        React UseEffect Fetch Data
+      </h1>
+      <Posts />
     </>
   );
 }
--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Posts = () => {
   const [Posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
-  return <div></div>;
+
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        " https://jsonplaceholder.typicode.com/posts ⁠"
+      );
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("error fetching posts:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="p-6 max-w-3xl mx-auto">Posts</h1>
+      <button
+        className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={fetchPosts}
+      >
+        Refresh
+      </button>
+    </div>
+  );
 };
 
 export default Posts;
