018825e6aba6fef7c109afab8a5c0eeb85100f56
Prisca <priscamasereli@gmail.com>
2025-09-01 11:18:04 +0200

    Feat: Final adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Posts from "./Components/Posts";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">
+      <h1 className="text-3xl font-bold p-10 text-center">
         React UseEffect Fetch Data
       </h1>
       <Posts />
