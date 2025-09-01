43fdf35a02301ba08c84061bdf1c11c0470b57ae
Prisca <priscamasereli@gmail.com>
2025-09-01 11:15:27 +0200

    Feat: Add loading the posts abilities and styles

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const Posts = () => {
-  const [Posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const fetchPosts = async () => {
     try {
       setLoading(true);
       const response = await fetch(
-        " https://jsonplaceholder.typicode.com/posts ⁠"
+        "https://jsonplaceholder.typicode.com/posts"
       );
+
       const data = await response.json();
       setPosts(data);
     } catch (error) {
@@ -24,14 +25,27 @@ const Posts = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="p-6 max-w-3xl mx-auto">Posts</h1>
-      <button
-        className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={fetchPosts}
-      >
-        Refresh
-      </button>
+    <div className="p-6 max-w-3xl mx-auto">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold mb-4">Posts</h1>
+        <button
+          className="mb-4 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
+          onClick={fetchPosts}
+        >
+          Refresh
+        </button>
+      </div>
+
+      {!loading && posts.length > 0 && (
+        <ul className="space-y-4">
+          {posts.slice(0, 10).map((post) => (
+            <li key={post.id} className="p-4 border rounded-lg shadow-sm">
+              <h2 className="font-semibold">{post.title}</h2>
+              <p className="text-gray-700">{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
