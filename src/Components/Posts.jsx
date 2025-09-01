03f3efa5dd739b3fb6ea3a33d5ffb079581e604a
Prisca <priscamasereli@gmail.com>
2025-09-01 10:35:08 +0200

    Feat: Creates Hooks

---
+++ src/Components/Posts.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Posts = () => {
+  const [Posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  return <div></div>;
+};
+
+export default Posts;
